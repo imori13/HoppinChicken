1f8a55faff210221abf6cb428adfc7c4714a3a75
鶴見昌之 <tsuruwatcher@gmail.com>
2019-10-18 15:43:25 +0900

    重み付きランダム用クラスと処理を追加

--- FliedChicken/Utilities/RandomSelector.cs
+++ FliedChicken/Utilities/RandomSelector.cs
@@ -14,5 +14,27 @@ namespace FliedChicken.Utilities
         {
             return collection.ElementAt(GameDevice.Instance().Random.Next(0, collection.Count()));
         }
+
+        public static T WeightSelect<T>(ICollection<WeightSelectHelper<T>> collection)
+        {
+            var random = GameDevice.Instance().Random;
+            int weightSum = collection.Sum(select => select.Value);
+            int randomValue = random.Next(1, weightSum + 2);
+
+            int index = 0;
+
+            for (int i = 0; i < collection.Count; i++)
+            {
+                if (collection.ElementAt(i).Value >= randomValue)
+                {
+                    index = i;
+                    break;
+                }
+                randomValue -= collection.ElementAt(i).Value;
+            }
+
+            return collection.ElementAt(index).SelectObject;
+    
+        }
     }
 }
